d96e818bd2c312a8a49a557ed2a02c50b8e4d113
MateusGoulart-1722 <mateusgoulart@etec.sp.gov.br>
2025-12-02 14:39:02 -0300

    tela vegana pronta

--- app/src/main/java/com/example/essenciavital/ELogicaReceitaVegana.java
+++ app/src/main/java/com/example/essenciavital/ELogicaReceitaVegana.java
@@ -1,6 +1,10 @@
 package com.example.essenciavital;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ELogicaReceitaVegana extends AppCompatActivity {
-
+    Button botao_var;
+    Button botao_var_2;
+    Button botao_var_3;
+    Button botao_var_4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,17 @@ public class ELogicaReceitaVegana extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        botao_var = findViewById(R.id.btnVerQuest);
+        botao_var_2 = findViewById(R.id.btn_verreceita1);
+        botao_var_3 = findViewById(R.id.btn_verreceita2);
+        botao_var_4 = findViewById(R.id.btn_verreceita3);
+        botao_var.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mudar_tela_var = new Intent(ELogicaReceitaVegana.this, DLogicaQuestionario.class);
+                startActivity(mudar_tela_var);
+            }
         });
     }
 }
